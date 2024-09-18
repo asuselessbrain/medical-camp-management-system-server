0ddb865d3cb9e127532ba9d30025a7bacfe9ae0e
asuselessbrain <shovon1023456789@gmail.com>
2024-09-18 17:05:30 +0600

    delete user from database relate api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 var cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = 3000;
 
@@ -65,6 +65,17 @@ async function run() {
         },
       };
       const result = await usersCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    // delete user from database
+
+    app.delete("/delete-user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await usersCollection.deleteOne(query);
+
       res.send(result);
     });
 
